4edb7245a92c10c134e0d241a3bf23db54e2bff5
K1NGAARON <gerardaaron1997@gmail.com>
2023-03-25 12:26:42 +0100

    base files

---
+++ js/app.js
@@ -0,0 +1,22 @@
+// STICKY LARGE MENU
+$(document).scroll(function () {
+    const header = $('.header');
+    const logo = $('.header .logo');
+
+    if ($(this).scrollTop() > header.height()) {
+        $(logo).attr("src","/branding/logo/logo-black.png");
+        header.addClass('active');
+    } else {
+        $(logo).attr("src","/branding/logo/logo-white.png");
+        header.removeClass('active');
+    }
+});
+
+// SMALL MENU
+$("#menu-toggle").click(function() {
+    $(".small-menu-wrapper").toggle("active");
+});
+
+function shuffleArray(array) {
+    array.sort(() => Math.random() - 0.5);
+};
